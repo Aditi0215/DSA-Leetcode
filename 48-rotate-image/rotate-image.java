bca342b277c266ba214600d90cd1fd4b556c007d
Aditi Awatade <122290608+Aditi0215@users.noreply.github.com>
2026-05-06 16:50:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (92.18%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -11,7 +11,7 @@ class Solution {
         }
 
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix.length;j++){
+            for(int j=0;j<matrix[i].length;j++){
                 matrix[i][j] = ans[i][j];
             }
         }
