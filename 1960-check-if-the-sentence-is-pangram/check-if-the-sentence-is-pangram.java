2215e6b0e09a4333708de17552ff16740a7b645d
Aditi Awatade <122290608+Aditi0215@users.noreply.github.com>
2026-06-28 23:20:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (83.95%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,19 +1,26 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean isFound[] = new boolean[26];
-        boolean isPanagram = true;
-        String s = sentence.toLowerCase();
+        for(char i='a';i<='z';i++){
+            if(sentence.indexOf(i)==-1)
+                return false;
+        }
+        return true;
 
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            isFound[ch-'a'] = true;
-        }
-        for(int i=0;i<26;i++){
-            if(isFound[i]==false){
-                isPanagram = false;
-                break;
-            }
-        }
-        return isPanagram;
+
+        // boolean isFound[] = new boolean[26];
+        // boolean isPanagram = true;
+        // String s = sentence.toLowerCase();
+
+        // for(int i=0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     isFound[ch-'a'] = true;
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(isFound[i]==false){
+        //         isPanagram = false;
+        //         break;
+        //     }
+        // }
+        // return isPanagram;
     }
 }
