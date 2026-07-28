8fcb8889fed3f2b51b4f1b5e62c93652fcc6a953
Aditi Awatade <122290608+Aditi0215@users.noreply.github.com>
2026-07-28 13:00:59 +0530

    Time: 1 ms (97.59%) | Memory: 47.4 MB (17.84%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
         int[] result = new int[nums.length * 2];
-
-        // Copy the array twice
-        System.arraycopy(nums, 0, result, 0, nums.length);
-        System.arraycopy(nums, 0, result, nums.length, nums.length);
-
+        for(int i=0;i<nums.length;i++){
+            for(int j=nums.length;j<result.length;j++){
+                result[i] = nums[i];
+                result[j] = nums[i++];
+            }
+        }
         return result;
     }
 }
